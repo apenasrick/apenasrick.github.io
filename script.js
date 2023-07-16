39eb6c672ad728b8591c2958c35532f0d098b58b
ApenasRick <54453569+apenasrick@users.noreply.github.com>
2023-07-16 00:02:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const emailInput = document.querySelector("#email");
-const emailCopyButton = document.querySelector("#email-copy-button");
 
-emailCopyButton.addEventListener("click", () => {
-  emailInput.select();
-  document.execCommand("copy");
-});
